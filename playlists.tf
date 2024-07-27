b0ddd092386879ae7a805e8e684e2f183a0492db
Omkar Nitin Pathak <omiipathak2000@gmail.com>
2024-07-27 17:27:19 +0530

    added the configuration files

---
+++ playlists.tf
@@ -0,0 +1,4 @@
+resource "spotify_playlist" "my_playlist" {
+  name = "Discipline"
+  tracks = ["3FqtduiaqnFYvBgKuc6QWQ","1BpvuKIG7jK5EiTp2tv5z8"]
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "apikey" {
+    type = string
+    default = "lZMpLwKDpBI_rey3U3D6qRLBQn10ubTQvTIBUBmRvPJfks6nfTxVzheKfifmn9Vi"
+}
